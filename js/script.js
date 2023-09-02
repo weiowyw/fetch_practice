06135239b9834d49d74ad7234e68100ae8f9211d
weiowyw <haxjlebatop@gmail.com>
2023-09-02 19:25:26 +0500

    ffds

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+//18.
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then((res) => res.json())
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+
+//19.
+// const res = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return await response.json();
+// }
+//
+// console.log(res());
